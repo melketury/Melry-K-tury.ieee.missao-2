30ac6f649a49da84765039dc486637881954ac89
melketury <162588365+melketury@users.noreply.github.com>
2024-03-26 08:36:48 -0300

    Add files via upload

---
+++ números com os dedos.py
@@ -0,0 +1,36 @@
+import cv2
+import mediapipe as mp
+
+video = cv2.VideoCapture(0)
+
+hand = mp.solutions.hands
+Hand = hand.Hands(max_num_hands=1)
+mpDraw = mp.solutions.drawing_utils
+
+while True:
+    check, img = video.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = Hand.process(imgRGB)
+    handPoints = results.multi_hand_landmarks
+    h, w, _ = img.shape
+    pontos = []  
+    if handPoints:
+        for points in handPoints:
+            mpDraw.draw_landmarks(img, points, hand.HAND_CONNECTIONS)
+            for id, cord in enumerate(points.landmark):
+                cx, cy = int(cord.x * w), int(cord.y * h)
+                #cv2.putText(img, str(id), (cx, cy + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+                pontos.append((cx, cy))
+        dedos = (8, 12, 16, 20)
+        contador = 0
+        if pontos:
+            if pontos[4][0] < pontos[2][0]:
+                contador += 1
+            for x in dedos:
+                if pontos[x][1] < pontos[x - 2][1]:
+                    contador += 1  
+        
+        cv2.putText(img, str(contador), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 4, (255, 0, 0), 5)
+        print(contador)     
+    cv2.imshow("image", img)
+    cv2.waitKey(1)
